52713121a86c339536418641de90d0b77379600e
XiomaraAmbar <xiomara.ortiz@estudiante.ucsm.edu.pe>
2025-05-20 07:47:09 -0500

    Adición del método insert e insertRecursivo mas el método interno searchInterno

--- bstreelinklistinterfgeneric/LinkedBST.java
+++ bstreelinklistinterfgeneric/LinkedBST.java
@@ -15,12 +15,12 @@ public class LinkedBST<E extends Comparable<E>> implements BinarySearchTree<E>{
 
     //Elimina todos los elementos del BST dejándolo vacío
     public void destroy(){
-        raiz = null; //Raiz esta vacío
+        raiz = null; //Raiz esta vacía, árbol vacío
     }
 
     //Verifica si el BST está vacío
     public boolean isEmpty(){
-        return raiz == null; //Si la raiz es null esta vacío
+        return raiz == null; //Si la raiz es null, el árbol esta vacío
     }
 
     //Busca un elemento en el BST
@@ -53,20 +53,56 @@ public class LinkedBST<E extends Comparable<E>> implements BinarySearchTree<E>{
         }
     }
 
-    //Agrega un elemento en el BST
-    public void insert(E valor) throws ItemDuplicated {
-        if (isEmpty()){
-            Nodo<E> nuevoNodo = new Nodo<E> (valor);
-            raiz = nuevoNodo;
-        }
-        else{
+    /*
+    Método de búsqueda privada para usarlo en insert y delete
+     */
+    private E searchInterno(E valor) {
+        return searchRecursivo(raiz,valor);
+    }
 
+    //Agrega un elemento en el BST
+    public void insert(E valor) throws ItemDuplicated{
+        if (isEmpty()){
+            raiz.setValor(valor);
+        }
+        else if (searchInterno(valor) != null){ //Busca si el elemento ya existe en el árbol
+            throw new ItemDuplicated();
+        }
+        insertRecursivo(raiz, valor);
+    }
+
+    //Busca recursivamente el valor de un nodo en el árbol, e inserta el valor en el lugar correcto
+    private void insertRecursivo(Nodo<E> nodoTemporal, E valor){
+        Nodo<E> nuevoNodo = new Nodo <> (valor);
+
+        int resultado = valor.compareTo(nodoTemporal.getValor());
+
+        if (resultado > 0){ //Si el valor a buscar es mayor al nodoTemporal (raizTemporal)
+            //Busca en la derecha(mayores)
+            if(nodoTemporal.getDerecho() == null){ //Si el hijo derecho del nodo esta vacío, se inserta
+                nodoTemporal.setDerecho(nuevoNodo);
+            }
+            else{
+                insertRecursivo(nodoTemporal.getDerecho(),valor); //nodoTemporal = nodoTemporal.getDerecho();
+            }
+        }
+
+        else { //Si el valor a buscar es menor al nodoTemporal (raizTemporal)
+            //Busca en la izquierda(menores)
+            if(nodoTemporal.getIzquierdo() == null){ //Si el hijo izquierdo del nodo esta vacío, se inserta
+                nodoTemporal.setIzquierdo(nuevoNodo);
+            }
+            else{
+                insertRecursivo(nodoTemporal.getIzquierdo(),valor); //nodoTemporal = nodoTemporal.getIzquierdo();
+            }
         }
     }
 
     //Elimina un elemento en el BST
     public void delete(E valor) throws ExceptionIsEmpty{
+        if (!(isEmpty())){
 
+        }
     }
 
     //Muestra la cadena que contiene la información en el BST
